901e568fe5e1c68a79250e8f0aabd7df493c6823
Rafael Bertti <rafaellsbertti@outlook.com>
2022-04-01 14:05:24 -0300

    Band Name Generator

---
+++ ProjectBandNameGenerator.py
@@ -0,0 +1,11 @@
+## Creating a greeting for the users
+print("Hi, create the name of your band with the Band Name Generator following the few steps")
+
+## First question for the users in the band generator
+q1_city = input("Which city did you grew up?\n")
+
+## Second question for the users in the band generator
+q2_pet = input("What is the name of your pet?\n")
+
+## Final result with the possible name for the band
+print("Your band name is: " + q1_city + " " + q2_pet)
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Hi, this is the Band Name Generator. Allows you to create your band name! 🤘🏻
+Hope you like it!
